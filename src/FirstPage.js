76d9f534897cc9ffde2a26b39465e8c1601cd3ab
hayoon0524 <92996412+hayoon0524@users.noreply.github.com>
2022-05-30 18:04:37 +0900

    Changes by gimtaehyun update

--- src/FirstPage.js
+++ src/FirstPage.js
@@ -2,8 +2,10 @@ import styles from './css/FirstPage.module.css'
 function FirstPage() {
     return (
         <div>
-            <h1>당신의 ?????을 저장하세요</h1>
-            <h1>간지 작렬하는 캐치프레이즈</h1>
+            <h1 className={styles.mainBody}>당신의 ?????을 저장하세요 </h1>
+            <h1 className={styles.mainBody}>간지 작렬하는 캐치프레이즈 </h1>
+
+            <h2 className={styles.mainBody}>계정 만들기</h2>
         </div>
     );
 }
