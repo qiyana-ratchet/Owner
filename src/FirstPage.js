d56fee4c0689ada17db19fcaa4229cb3b137439a
gimtaehyun <wereter31@naver.com>
2022-05-30 18:15:25 +0900

    update FirstPage.js

--- src/FirstPage.js
+++ src/FirstPage.js
@@ -1,11 +1,16 @@
 import styles from './css/FirstPage.module.css'
+
 function FirstPage() {
     return (
-        <div>
-            <h1 className={styles.mainBody}>당신의 ?????을 저장하세요 </h1>
-            <h1 className={styles.mainBody}>간지 작렬하는 캐치프레이즈 </h1>
+        <body>
+            <div>
+                <h1 className={styles.catchphrase}>당신의 ?????을 저장하세요<br/>
+                    간지 작렬하는 캐치프레이즈</h1>
 
-            <h2 className={styles.mainBody}>계정 만들기</h2>
-        </div>
+            </div>
+        </body>
+
     );
 }
+
+export default FirstPage;
