8517ad71326cfcc8b0bd75b20168424aa202f410
gimtaehyun <wereter31@naver.com>
2022-06-01 14:35:14 +0900

    FirstPage 업데이트

--- src/FirstPage.js
+++ src/FirstPage.js
@@ -6,7 +6,7 @@ function FirstPage() {
             <div>
                 <h1 className={styles.catchphrase}>당신의 ?????을 저장하세요<br/>
                     간지 작렬하는 캐치프레이즈</h1>
-
+                <a href="" className={styles.joinButton}>계정 만들기</a>
             </div>
         </body>
 
