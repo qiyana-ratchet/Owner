e85ac1ef00b3822be8c1b2774c6d256e8377ecd7
hayoon0524 <92996412+hayoon0524@users.noreply.github.com>
2022-06-01 15:36:38 +0900

    css center align

--- src/FirstPage.js
+++ src/FirstPage.js
@@ -2,14 +2,11 @@ import styles from './css/FirstPage.module.css'
 
 function FirstPage() {
     return (
-        <body>
-            <div>
-                <h1 className={styles.catchphrase}>당신의 ?????을 저장하세요<br/>
-                    간지 작렬하는 캐치프레이즈</h1>
-                <a href="" className={styles.joinButton}>계정 만들기</a>
-            </div>
-        </body>
-
+        <div className={styles.main}>
+            <h1 className={styles.catchphrase}>당신의 ?????을 저장하세요<br/>
+                간지 작렬하는 캐치프레이즈</h1>
+            <button className={styles.joinButton}>계정 만들기</button>
+        </div>
     );
 }
 
