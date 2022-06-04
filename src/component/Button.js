4ab4c8d45620d57ceb4e8459915a7db040ed1432
qiyana-ratchet <wereter31@naver.com>
2022-06-04 19:10:19 +0900

    button : 버튼 추가

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,11 +1,22 @@
 import styles from "../css/Button.module.css"
+
 function Button(props) {
     return (
         <div>
-        <button className={ `${styles.design1} ${props.className}`}
-                onClick= {props.onClick}>
-            {props.text}
-        </button>
+            <button className={`${styles.design1} ${props.className}`}
+                    onClick={props.onClick}>
+                {props.text}
+            </button>
+        </div>
+    );
+}
+function Button2(props) {
+    return (
+        <div>
+            <button className={`${styles.design2} ${props.className}`}
+                    onClick={props.onClick}>
+                {props.text}
+            </button>
         </div>
     );
 }
