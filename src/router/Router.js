dff2ffd9c2c0569a0b051d66d499f3fd71b2b6c5
gimtaehyun <wereter31@naver.com>
2022-06-04 14:17:01 +0900

    login: 첫 화면 변경

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import FirstPage from "../page/FirstPage";
+import JoinPage from "../page/JoinPage";
 
 export default function Router() {
     return (
         <Routes>
-            <Route exact path='/' element={<FirstPage />} />
+            <Route exact path='/' element={<JoinPage></JoinPage>} />
         </Routes>
     );
 }
