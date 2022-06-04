039bac4394cd75e6f029969499f89e28d20290bb
qiyana-ratchet <wereter31@naver.com>
2022-06-04 19:12:31 +0900

    login: css 업데이트

--- src/page/JoinPage.js
+++ src/page/JoinPage.js
@@ -1,29 +1,31 @@
 import styles from '../css/FirstPage.module.css'
 import Button from "../component/Button";
-
+import React, { useState } from 'react';
 
 function JoinPage() {
-    function googleLogin() {
-        console.log("구글 로그인")
-    }
+
+
+
     return (
-        <div className={styles.main}>
-            <h2>Wishchiving</h2>
-            <h3>계정 만들기</h3>
-            <a>로그인하면 어쩌구저쩌구 동의하는 것이고...<br/></a>
-            <Button className={styles.googleLoginButton} text = "Google 계정으로 로그인 하기" onClick = {googleLogin}/>
-            <Button className={styles.appleLoginButton} text = "Apple 계정으로 로그인 하기" onClick = {googleLogin}/>
-            <Button className={styles.kakaoLoginButton} text = "Kakao 계정으로 로그인 하기" onClick = {googleLogin}/>
-            <hr/>
-            <Button className={styles.emailLoginButton} text = "휴대폰 번호 혹은 이메일로 가입하기" onClick = {googleLogin}/>
 
-            <a className={styles.loginWithAccount}>Google 계정으로 로그인 하기<br/></a>
-            <a className={styles.loginWithAccount}>Apple 계정으로 로그인 하기<br/></a>
-            <a className={styles.loginWithAccount}>Kakao 계정으로 로그인 하기<br/></a>
-            <hr/>
-            <a className={styles.loginWithAccount}>휴대폰 번호 혹은 이메일로 가입하기</a>
+    <div className={styles.main}>
 
-        </div>
+        <h2>Wishchiving</h2>
+        <h3>계정 만들기</h3>
+        <a>로그인하면 어쩌구저쩌구 동의하는 것이고...<br/></a>
+        <Button className={styles.googleLoginButton} text = "Google 계정으로 로그인 하기" onClick = {googleLogin}/>
+        <Button className={styles.appleLoginButton} text = "Apple 계정으로 로그인 하기" onClick = {googleLogin}/>
+        <Button className={styles.kakaoLoginButton} text = "Kakao 계정으로 로그인 하기" onClick = {googleLogin}/>
+        <hr/>
+        <Button className={styles.emailLoginButton} text = "휴대폰 번호 혹은 이메일로 가입하기" onClick = {googleLogin}/>
+
+        <a className={styles.loginWithAccount}>Google 계정으로 로그인 하기<br/></a>
+        <a className={styles.loginWithAccount}>Apple 계정으로 로그인 하기<br/></a>
+        <a className={styles.loginWithAccount}>Kakao 계정으로 로그인 하기<br/></a>
+        <hr/>
+        <a className={styles.loginWithAccount}>휴대폰 번호 혹은 이메일로 가입하기</a>
+
+    </div>
     );
 }
 
--- src/router/Router.js
+++ src/router/Router.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import FirstPage from "../page/FirstPage";
-import JoinPage from "../page/JoinPage";
 
 export default function Router() {
     return (
         <Routes>
-            <Route exact path='/' element={<JoinPage></JoinPage>} />
+            <Route exact path='/' element={<FirstPage></FirstPage>} />
         </Routes>
     );
 }
