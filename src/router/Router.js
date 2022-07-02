1086528176a4add374c535bc164978cf3a8e236c
qiyana-ratchet <wereter31@naver.com>
2022-07-02 21:13:44 +0900

    modal: 라우터 추가

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import {BrowserRouter, Routes, Route, NavLink, Redirect} from "react-router-dom";
 import FirstPage from "../page/FirstPage";
+import JoinPage from "../page/JoinPage";
 
 export default function Router() {
-    return (
-        <Routes>
-            <Route exact path='/' element={<FirstPage></FirstPage>} />
-        </Routes>
-    );
+
+  return (
+    <div className='App'>
+      <Routes>
+        <Route exact path='/' element={<FirstPage/>}/>
+        <Route exact path='/join' element={<JoinPage/>}/>
+      </Routes>
+    </div>);
 }
