06804efbca81cc421016de1ec3c7aa84634de77b
hayoon <hayoon0524@konkuk.ac.kr>
2022-07-06 17:05:05 +0900

    firstpage css flex change

--- src/page/FirstPage.js
+++ src/page/FirstPage.js
@@ -1,45 +1,33 @@
 import styles from '../css/FirstPage.module.css'
 import Button from "../component/Button";
-import React, { useState } from 'react';
-import Modal from '../component/Modals/Modal';
 
 function FirstPage() {
 
     function login() {
         console.log("로그인")
     }
+
     function register() {
         console.log("회원가입")
     }
 
-    const [modalOpen, setModalOpen] = useState(false);
-    const openModal = () => {
-        setModalOpen(true);
-        console.log("모달 열음")
-    };
-    const closeModal = () => {
-        setModalOpen(false);
-        console.log("모달 닫음")
-    };
-
-
     return (
-        <React.Fragment>
-            <Modal open={modalOpen} close={closeModal} header="">
-            </Modal>
-
-            <div className={styles.main}>
-                <h1 className={styles.catchphrase}>당신의 ?????을 저장하세요<br/>
-                    간지 작렬하는 캐치프레이즈</h1>
-                <Button className={styles.joinButton} text = "계정 만들기" onClick={ () => {
-                    register()
-                    openModal()
-                }}></Button>
-                <Button className={styles.loginButton} text = "로그인" onClick = {login}></Button>
-                <h3 className={styles.logo}>Wishchiving</h3>
-                <a className={styles.lookHomepage}>홈페이지 둘러보기</a>
-            </div>
-        </React.Fragment>
+        <div className={styles.main}>
+                <div className={styles.flex}>
+                    <div className={styles.row}>
+                        <h3 className={styles.logo}>Wishchiving</h3>
+                        <Button className={styles.loginButton} text="로그인" onClick={login}></Button>
+                    </div>
+                    <div className={styles.mid}>
+                        <h1 className={styles.catchphrase}>당신의 ?????을 저장하세요<br/>
+                            간지 작렬하는 캐치프레이즈</h1>
+                        <Button className={styles.joinButton} text="계정 만들기" onClick={register}></Button>
+                    </div>
+                    <div className={styles.bottom}>
+                        <p className={styles.lookHomepage}>홈페이지 둘러보기</p>
+                    </div>
+                </div>
+        </div>
     );
 }
 
